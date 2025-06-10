a8eac0187ee215bfb214da4e585d02827d698246
Rafael <rsa@inicie.digital>
2025-06-10 20:08:40 -0300

    ADD Loading support and interceptor

--- frontend-tekna/src/app/services/http.interceptor.ts
+++ frontend-tekna/src/app/services/http.interceptor.ts
@@ -1,15 +1,25 @@
 import {
+  HttpErrorResponse,
   HttpEvent,
   HttpHandler,
   HttpInterceptor,
   HttpRequest,
 } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import {
+  catchError,
+  delay,
+  Observable,
+  Subscription,
+  take,
+  throwError,
+} from 'rxjs';
 
 @Injectable()
 export class TokenInterceptor implements HttpInterceptor {
   token: any;
+
+  constructor() {}
 
   intercept(
     req: HttpRequest<unknown>,
@@ -20,13 +30,13 @@ export class TokenInterceptor implements HttpInterceptor {
     }
 
     if (this.token && !req?.url.includes('auth')) {
-        const modReq = req.clone({
-          setHeaders: {
-            Authorization: 'Bearer ' + this.token,
-          },
-        });
+      const modReq = req.clone({
+        setHeaders: {
+          Authorization: 'Bearer ' + this.token,
+        },
+      });
 
-        return next.handle(modReq);
+      return next.handle(modReq);
     }
     return next.handle(req);
   }
---
+++ frontend-tekna/src/app/services/loading/loading.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Router } from '@angular/router';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-loading',
+  templateUrl: './loading.component.html',
+  styleUrls: ['./loading.component.scss'],
+  imports: [ReactiveFormsModule, CommonModule],
+})
+export class LoadingComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
