2d3ac3f760eb9b6c3db46b5df3e36d8d67a58d01
Rafael <rsa@inicie.digital>
2025-06-11 19:55:49 -0300

    ADD Header component

--- frontend-tekna/src/app/pages/header/header.component.ts
+++ frontend-tekna/src/app/pages/header/header.component.ts
@@ -10,18 +10,27 @@ import {
 } from '@angular/forms';
 import { LoginService } from '../login/login.service';
 import { CommonModule } from '@angular/common';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
-  imports: [ReactiveFormsModule, CommonModule]
+  imports: [ReactiveFormsModule, CommonModule, FontAwesomeModule],
 })
 export class HeaderComponent implements OnInit {
+  faUser = faUser;
 
-  constructor(
-  ) {}
+  constructor(private router: Router) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  toUser() {
+    this.router.navigate(['user']);
+  }
+
+  toHome() {
+    this.router.navigate(['tasks']);
   }
 }
