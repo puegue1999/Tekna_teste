58a336c9d97b123a473ce5f404466d1083385589
Rafael <rsa@inicie.digital>
2025-06-11 19:58:34 -0300

    UPDATE Tables task with filters

--- frontend-tekna/src/app/pages/tasks/tasks.component.ts
+++ frontend-tekna/src/app/pages/tasks/tasks.component.ts
@@ -5,44 +5,144 @@ import { CommonModule } from '@angular/common';
 import { Task } from './tasks';
 import { LoadingComponent } from '../../shared/loading/loading.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faEye, faTrash } from '@fortawesome/free-solid-svg-icons';
+import {
+  faEye,
+  faTrash,
+  faArrowLeft,
+  faArrowRight,
+} from '@fortawesome/free-solid-svg-icons';
+import { ModalComponent } from '../../shared/modal/modal.component';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { debounceTime, distinctUntilChanged, filter } from 'rxjs';
 
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
   styleUrls: ['./tasks.component.scss'],
-  imports: [CommonModule, LoadingComponent, FontAwesomeModule],
+  imports: [
+    CommonModule,
+    LoadingComponent,
+    FontAwesomeModule,
+    ModalComponent,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
 })
 export class TasksComponent implements OnInit {
   faEye = faEye;
   faTrash = faTrash;
+  faArrowLeft = faArrowLeft;
+  faArrowRight = faArrowRight;
   userToken?: string;
   listTasks: Task[] = [];
+  page: number = 1;
+  totalPages: number = 1;
   isLoading: boolean = true;
+  modalOpen: boolean = false;
+  modalMessage: string = 'Deseja excluir esta task?';
+  modalBackButton: boolean = true;
+  deleteExternalId: string = '';
+  orderByOptions = [
+    { label: 'Title', value: 'title' },
+    { label: 'Expiration At', value: 'expirationAt' },
+  ];
+  selectedOrderBy: string = 'title';
+  orderDirectionOptions = [
+    { label: 'Ascending', value: 'asc' },
+    { label: 'Descending', value: 'desc' },
+  ];
+  selectedOrderDirection: string = 'asc';
+  finishedOptions = [
+    { label: 'Completed', value: 'finished' },
+    { label: 'Pending', value: 'pending' },
+    { label: 'No select', value: 'noSelect' },
+  ];
+  selectedFinished: string = 'noSelect';
+  search = new FormControl('');
 
   constructor(private router: Router, private tasksService: TasksService) {}
 
   ngOnInit(): void {
     this.userToken = localStorage.getItem('user') ?? undefined;
     this.getAllTasks();
+    this.search.valueChanges
+      .pipe(
+        debounceTime(500),
+        distinctUntilChanged()
+      )
+      .subscribe((value) => {
+        this.getOptions();
+      });
+  }
+
+  previousPage() {
+    this.page--;
+    this.getAllTasks();
+  }
+
+  nextPage() {
+    this.page++;
+    this.getAllTasks();
+  }
+
+  getOptions() {
+    this.page = 1;
+    this.getAllTasks();
   }
 
   getAllTasks() {
-    this.tasksService.getAllTasks(this.userToken).subscribe({
+    this.isLoading = true;
+    this.tasksService
+      .getAllTasks(
+        this.userToken,
+        this.page,
+        this.selectedOrderBy,
+        this.selectedOrderDirection,
+        this.selectedFinished,
+        this.search.value === '' ? 'all' : this.search.value
+      )
+      .subscribe({
+        next: (data) => {
+          this.listTasks = (data.allTasks.tasks ?? []).map((task: Task) => ({
+            externalId: task.externalId,
+            title: task.title,
+            description: task.description,
+            expirationAt: new Date(task.expirationAt),
+            finished: task.finished,
+          }));
+          this.totalPages = data.allTasks.totalPages;
+          this.isLoading = false;
+        },
+        error: (err) => {
+          this.isLoading = false;
+        },
+      });
+  }
+
+  openModalDelete(externalId: string) {
+    this.modalOpen = true;
+    this.deleteExternalId = externalId;
+  }
+
+  handleClose(shouldUpdate: boolean) {
+    this.modalOpen = false;
+    if (shouldUpdate) {
+      this.deleteTask();
+    }
+  }
+
+  deleteTask() {
+    this.isLoading = true;
+    this.tasksService.deleteTasks(this.deleteExternalId).subscribe({
       next: (data) => {
-        this.listTasks = (data.allTasks ?? []).map((task: Task) => ({
-          externalId: task.externalId,
-          title: task.title,
-          description: task.description,
-          expirationAt: new Date(task.expirationAt),
-          finished: task.finished,
-        }));
         this.isLoading = false;
-        console.log(this.listTasks);
+        this.deleteExternalId = '';
+        this.getAllTasks();
       },
       error: (err) => {
         console.error('Erro ao buscar tarefas', err);
         this.isLoading = false;
+        this.deleteExternalId = '';
       },
     });
   }
--- frontend-tekna/src/app/pages/tasks/tasks.service.ts
+++ frontend-tekna/src/app/pages/tasks/tasks.service.ts
@@ -8,8 +8,17 @@ import { Observable } from 'rxjs';
 export class TasksService {
   constructor(private http: HttpClient) {}
 
-  getAllTasks(user: any): Observable<any> {
-    return this.http.get(`http://localhost:3000/tasks/${user}`);
+  getAllTasks(
+    user: any,
+    page: any,
+    orderBy: any,
+    orderDirection: any,
+    finished: any,
+    search: any
+  ): Observable<any> {
+    return this.http.get(
+      `http://localhost:3000/tasks/${user}/${page}/${orderBy}/${orderDirection}/${finished}/${search}`
+    );
   }
 
   getTasks(userId: any, externalId: any): Observable<any> {
@@ -21,6 +30,13 @@ export class TasksService {
   }
 
   updateTasks(userId: any, externalId: any, data: any): Observable<any> {
-    return this.http.patch(`http://localhost:3000/tasks/${userId}/${externalId}`, data);
+    return this.http.patch(
+      `http://localhost:3000/tasks/${userId}/${externalId}`,
+      data
+    );
+  }
+
+  deleteTasks(externalId: any): Observable<any> {
+    return this.http.delete(`http://localhost:3000/tasks/${externalId}`);
   }
 }
